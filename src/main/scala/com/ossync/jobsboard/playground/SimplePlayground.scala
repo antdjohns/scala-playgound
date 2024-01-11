70401d58a2e0aee89c043cbc03068024cd4131c7
anthony johnson <114414459+antdjohns@users.noreply.github.com>
2024-01-10 19:58:55 -0800

    Stub Endpoints and Config Setup

--- src/main/scala/com/ossync/jobsboard/playground/SimplePlayground.scala
+++ src/main/scala/com/ossync/jobsboard/playground/SimplePlayground.scala
@@ -1,7 +1,20 @@
 package com.ossync.jobsboard.playground
 
+import cats.effect.*
+import cats.effect.implicits.*
+import cats.effect.unsafe.implicits.global
+
 object SimplePlayground {
   def main(args: Array[String]): Unit = {
-    println("Will rock the jvm")
+    def fib(n: Int, a: Long = 0, b: Long = 1): IO[Long] = {
+      IO(a + b ).flatMap { b2 => 
+        if (n > 0) fib(n - 1, b, b2) 
+        else IO.pure(b2)
+      }
+  }
+
+  val a = fib(10)
+  println(a.unsafeRunSync())
+  println("success")
   }
 }
