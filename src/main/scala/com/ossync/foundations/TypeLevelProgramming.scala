8d0546c6b51d5a27198e1c7c1277a3fbc37f1968
anthony johnson <114414459+antdjohns@users.noreply.github.com>
2024-01-14 17:32:01 -0800

    Adding syntax

---
+++ src/main/scala/com/ossync/foundations/TypeLevelProgramming.scala
@@ -0,0 +1,39 @@
+package com.ossync.foundations
+
+object TypeLevelProgramming  extends App {
+  // Design High level list as a typed level list
+  type DInt = Int
+  val a: DInt = 1
+  // Peano Arithmatic
+  trait Nat
+  
+
+  
+  class Succ[N <: Nat] extends Nat
+  
+  class _0 extends Nat
+  
+  type _1  = Succ[_0]
+  type _2 = Succ[_1]
+  type _3 = Succ[_2]
+  type _4 = Succ[_3]
+  type _5 = Succ[_4]
+
+  trait <[A <: Nat, B <: Nat]
+  object < {
+    given basic[N <: Nat]: <[_0, Succ[N]] with {}
+    given inductive[A <: Nat, B <: Nat](using <[A, B]): <[Succ[A], Succ[B]] with {}
+    def apply[A <: Nat, B <: Nat](using lt: <[A, B]): <[A, B] = lt
+  }
+
+  val zeroLessThanTwo = <[_0, _2]
+  val threeLessThanOne = <[_3, _1]
+
+  case class Person(name: String, age: Int)
+  given p: Person = Person("sam", 1)
+  def printName(using person: Person): Unit = {
+    println(s"${person.name} ${person.age}")
+  }
+
+  printName
+}
--- src/main/scala/com/ossync/jobsboard/config/syntax.scala
+++ src/main/scala/com/ossync/jobsboard/config/syntax.scala
@@ -3,8 +3,17 @@ package com.ossync.jobsboard.config
 import pureconfig.ConfigSource
 import cats.MonadThrow
 import pureconfig.ConfigReader
+import cats.implicits.*
+import com.typesafe.config.ConfigException
+import pureconfig.error.ConfigReaderException
+import scala.reflect.ClassTag
 
 object syntax {
   extension (source: ConfigSource)
-    def loadF[F[_], A](using reader: ConfigReader[A], F: MonadThrow[F]): F[A] = ???
+    def loadF[F[_], A](using reader: ConfigReader[A], F: MonadThrow[F], tag: ClassTag[A]): F[A] = {
+        F.pure(source.load[A]).flatMap {
+            case Left(errors) => F.raiseError[A](ConfigReaderException(errors))
+            case Right(value) => F.pure(value)
+        }
+    }
 }
